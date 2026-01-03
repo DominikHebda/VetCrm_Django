176ce19860bfbe8fc2993d7171eba9bdcf55ac98
DominikHebda <hebdad@wp.pl>
2026-01-03 12:19:09 +0100

    Register Appointment model in admin.

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from .models import Pet
+from .models import Pet, Appointment
+
 
 # Register your models here.
 @admin.register(Pet)
 class PetAdmin(admin.ModelAdmin):
     list_display = ('name', 'species', 'owner_name', 'owner_phone')
     search_fields = ('name', 'owner_name')
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('pet', 'date', 'reason')
+    list_filter = ('date',)
+    search_fields = ('pet', 'reason')
