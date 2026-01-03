3e3a1b3ecb58327de0bc67f63db6cdcdc23f330e
DominikHebda <hebdad@wp.pl>
2026-01-03 11:35:56 +0100

    Register Pet model in admin, Rename Pets model to Pet and fix admin naming.

--- core/models.py
+++ core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Pets(models.Model):
+class Pet(models.Model):
     name = models.CharField(max_length=100)
     species = models.CharField(max_length=100)
     breed = models.CharField(max_length=100, blank=True, null=True)
@@ -12,6 +12,10 @@ class Pets(models.Model):
     owner_phone = models.CharField(max_length=20)
 
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Zwierzę"
+        verbose_name_plural = "Zwierzęta"
 
     def __str__(self):
         return f"{self.name} ({self.species}) {self.breed} ({self.birth_date})"
