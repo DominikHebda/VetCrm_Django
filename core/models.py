c4642c4c549ced0e61172cdbd3089a3e2691e8d8
DominikHebda <hebdad@wp.pl>
2026-01-03 12:12:48 +0100

    Add Appointment model

--- core/models.py
+++ core/models.py
@@ -19,3 +19,24 @@ class Pet(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.species}) {self.breed} ({self.birth_date})"
+
+
+class Appointment(models.Model):
+    pet = models.ForeignKey(
+        Pet,
+        on_delete=models.CASCADE,
+        related_name='appointments'
+    )
+    date = models.DateTimeField()
+    reason = models.CharField(max_length=255)
+    notes = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Wizyta"
+        verbose_name_plural = "Wizyty"
+        ordering = ["-date"]
+
+    def __str__(self):
+        return f"{self.pet.name} {self.date:%Y-%m-%d %H:%M}"
