5c23ec5a710b568124985a55a341fde502c12198
DominikHebda <hebdad@wp.pl>
2026-01-03 11:12:26 +0100

    Add Pets model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Pets(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100, blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+
+    owner_name = models.CharField(max_length=100)
+    owner_email = models.EmailField(blank=True, null=True)
+    owner_phone = models.CharField(max_length=20)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.species}) {self.breed} ({self.birth_date})"
